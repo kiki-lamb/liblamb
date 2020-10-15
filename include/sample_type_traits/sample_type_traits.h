f7e391ce9d035f7ac843132426a2e6adeca4148c
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-15 18:02:41 -0400

    ...

--- include/sample_type_traits/sample_type_traits.h
+++ include/sample_type_traits/sample_type_traits.h
@@ -23,18 +23,23 @@ namespace lamb {
     typedef int16_t mix_type;
     typedef uint8_t unsigned_type;
     typedef int8_t signed_type;
-    static const int16_t maximum = 127;
-    static const int16_t silence = 0;
-    static const int16_t minimum = -128;
-    static const int16_t bias_to_signed = 0;
+
+    static const bool    is_signed        = true;    
+    static const int16_t maximum          = 127;
+    static const int16_t silence          = 0;
+    static const int16_t minimum          = -128;
+    static const int16_t bias_to_signed   = 0;
     static const int16_t bias_to_unsigned = 128;
 //    static const value_type * sine_table;
+
     static inline uint8_t to_uint8_t(value_type v) {
       return v + bias_to_unsigned;
     }
+
     static inline int8_t to_int8_t(value_type v) {
       return v;
     }
+
     static inline value_type id(value_type x) { return x; };
   };
 
@@ -48,16 +53,20 @@ namespace lamb {
     typedef int32_t mix_type;
     typedef uint16_t unsigned_type;
     typedef int16_t signed_type;
-    static const int16_t maximum = 32767;
-    static const int16_t silence = 0;
-    static const int16_t minimum = -32768;
-    static const int16_t bias_to_signed = 0;
+
+    static const bool    is_signed        = true;
+    static const int16_t maximum          = 32767;
+    static const int16_t silence          = 0;
+    static const int16_t minimum          = -32768;
+    static const int16_t bias_to_signed   = 0;
     static const int16_t bias_to_unsigned = -32768;
 //    static const value_type * sine_table;
-    static inline uint8_t to_uint8_t(value_type v) {
+
+    static inline uint16_t to_uint8_t(value_type v) {
       return v + bias_to_unsigned;
     }
-    static inline int8_t to_int8_t(value_type v) {
+
+    static inline int16_t to_int8_t(value_type v) {
       return v;
     }
     static inline value_type id(value_type x) { return x; };
@@ -73,15 +82,19 @@ namespace lamb {
     typedef uint16_t mix_type;
     typedef uint8_t unsigned_type;
     typedef int8_t signed_type;
-    static const int16_t maximum = 255;
-    static const int16_t silence = 128;
-    static const int16_t minimum = 0;
-    static const int16_t bias_to_signed = -128;
+
+    static const bool    is_signed        = true;
+    static const int16_t maximum          = 255;
+    static const int16_t silence          = 128;
+    static const int16_t minimum          = 0;
+    static const int16_t bias_to_signed   = -128;
     static const int16_t bias_to_unsigned = 0;
 //    static const value_type * sine_table;
+
     static inline uint8_t to_uint8_t(value_type v) {
       return v;
     }
+
     static inline int8_t to_int8_t(value_type v) {
       return v + bias_to_signed;
     }
