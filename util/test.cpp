ac8292aa0966b59a86bc369f57067c7affb6a93b
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-21 02:52:16 -0400

    ...

--- util/test.cpp
+++ util/test.cpp
@@ -16,12 +16,12 @@
 // s0,15   Yes
 // s0,31   Yes
 // s7,8    Yes
-// s15,16  
+// s15,16  Yes
 
 using namespace lamb;
 
-typedef signed_frac<15,16>     l_test_t;
-typedef unsigned_frac<0,8>   r_test_t;
+typedef unsigned_frac<0,16>   l_test_t;
+typedef unsigned_frac<0,16>   r_test_t;
 
 #define L_BASE ((l_test_t::MAX >> (sizeof(l_test_t::type) >> 1) >> 4) + 1)
 #define R_BASE ((r_test_t::MAX >> (sizeof(r_test_t::type) >> 1) >> 5) + 1)
