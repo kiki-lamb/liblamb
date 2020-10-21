aed497057c88156a40c5be949f66c2f6fdea3f47
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-20 21:07:16 -0400

    seems okay

--- util/test.cpp
+++ util/test.cpp
@@ -6,8 +6,8 @@
 
 using namespace lamb;
 
-typedef signed_frac<0,7>   l_test_t;
-typedef unsigned_frac<0,8> r_test_t;
+typedef signed_frac<0,15>   l_test_t;
+typedef unsigned_frac<0,16> r_test_t;
 
 #define BASE 1
 
