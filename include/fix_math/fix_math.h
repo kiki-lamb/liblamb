4c19abda703403a3fc0ce0dd6545776860bbef73
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-21 00:20:39 -0400

    ...

--- include/fix_math/fix_math.h
+++ include/fix_math/fix_math.h
@@ -318,14 +318,17 @@ namespace lamb {
       unsigned_frac operator * (unsigned_frac<charac,mantissa> const & other ) {
       big_type      tmp = (((big_type)val) * other.val) >> unsigned_frac<charac,mantissa>::FX_SHIFT;
       unsigned_frac r   = unsigned_frac((type)tmp);
-      
+
+      printf("TMP   is %lld.\n", tmp);
+      printf("r.val is %u.\n", r.val);
+
       if (tmp > ONE) {
 #ifndef LAMB_FP_SATURATE
-        printf("OVERFLOW: %d * %d = %d\n", val, other.val, tmp);
+        printf("OVERFLOW: %d * %d = %lld\n", val, other.val, tmp);
         fflush(stdout);
 #else
         r.val = ONE;
-        printf("SAT HI:  %d * %d = %d\n", val, other.val, r.val);
+        printf("SAT HI:  %d * %d = %lld\n", val, other.val, r.val);
 #endif
       }        
       return r;
@@ -874,7 +877,7 @@ namespace lamb {
 
     template <uint8_t charac, uint8_t mantissa>
     signed_frac operator * (unsigned_frac<charac,mantissa> const & other ) {
-      big_type    tmp = (((big_type)val) * other.val) >> (unsigned_frac<charac,mantissa>::FX_SHIFT - 1);
+      big_type    tmp = (((big_type)val) * other.val) >> unsigned_frac<charac,mantissa>::FX_SHIFT;
       signed_frac r   = signed_frac((type)tmp);
       
       if (tmp > ONE) {
@@ -999,7 +1002,7 @@ namespace lamb {
 
     template <uint8_t charac, uint8_t mantissa>
     signed_frac operator * (unsigned_frac<charac,mantissa> const & other ) {
-      big_type    tmp = (((big_type)val) * other.val) >> (unsigned_frac<charac,mantissa>::FX_SHIFT - 1);
+      big_type    tmp = (((big_type)val) * other.val) >> unsigned_frac<charac,mantissa>::FX_SHIFT;
       signed_frac r   = signed_frac((type)tmp);
       
       if (tmp > ONE) {
@@ -1125,7 +1128,7 @@ namespace lamb {
 
     template <uint8_t charac, uint8_t mantissa>
     signed_frac operator * (unsigned_frac<charac,mantissa> const & other ) {
-      big_type    tmp = (((big_type)val) * other.val) >> (unsigned_frac<charac,mantissa>::FX_SHIFT - 1);
+      big_type    tmp = (((big_type)val) * other.val) >> unsigned_frac<charac,mantissa>::FX_SHIFT;
       signed_frac r   = signed_frac((type)tmp);
       
       if (tmp > ONE) {
--- util/test.cpp
+++ util/test.cpp
@@ -4,29 +4,52 @@
 // g++ -std=gnu++14 -DNO_ARDUINO -DLAMB_FP_SATURATE test.cpp && ./a.out
 // g++ -std=gnu++14 -DNO_ARDUINO test.cpp && ./a.out
 
+// Multiply tests:
+//
+// L/R     u0,8   u0,16  u0,32  u16,16  s0,7  s0,15  s0,31  s7,8  s15,16
+// u0,8    Yes
+// u0,16   Yes
+// u0,32   Yes
+// u8,8
+// u16,16
+// s0,7
+// s0,15
+// s0,31
+// s7,8
+// s15,16
+
 using namespace lamb;
 
-typedef signed_frac<0,15>    l_test_t;
+typedef unsigned_frac<0,32>  l_test_t;
 typedef unsigned_frac<0,8>   r_test_t;
 
-#define L_BASE 64
-#define R_BASE 4
+#define L_BASE ((l_test_t::MAX >> (sizeof(l_test_t::type) >> 1) >> 5) + 1)
+#define R_BASE ((r_test_t::MAX >> (sizeof(r_test_t::type) >> 1) >> 5) + 1)
 
 int main() {
-  printf("l ONE is %d.\n", l_test_t::ONE);
-  printf("l MAX is %d.\n", l_test_t::MAX);
-  printf("l MIN is %d.\n", l_test_t::MIN);
-  printf("r ONE is %d.\n", r_test_t::ONE);
-  printf("r MAX is %d.\n", r_test_t::MAX);
-  printf("r MIN is %d.\n", r_test_t::MIN);
+  printf("THIS   is %u.\n", UINT32_MAX);
+  printf("l BASE is %u.\n", L_BASE);
+  printf("l ONE  is %u.\n", l_test_t::ONE);
+  printf("l MAX  is %u.\n", l_test_t::MAX);
+  printf("l MIN  is %u.\n", l_test_t::MIN);
+  printf("r BASE is %u.\n", R_BASE);
+  printf("r ONE  is %u.\n", r_test_t::ONE);
+  printf("r MAX  is %u.\n", r_test_t::MAX);
+  printf("r MIN  is %u.\n", r_test_t::MIN);
   
   for (  l_test_t::big_type x = l_test_t::MIN; x <= l_test_t::MAX; x += L_BASE) {
-    printf("[%d]\n", x);
+    printf("[%lld]\n", x);
     
     for (  r_test_t::big_type y = r_test_t::MIN; y <= r_test_t::MAX; y += R_BASE) {    
       l_test_t fx(x);
       r_test_t fy(y);
       
+      {
+        l_test_t fz = fx * fy;
+        printf("Try %lld * %d: = %u \n", x, y, fz.val);
+        fflush(stdout);
+      }
+
       // {
       //   test_t fz = fx + fy;
       //   printf("Try %d + %d: = %d \n", x, y, fz.val);
@@ -39,12 +62,6 @@ int main() {
       //   fflush(stdout);
       // }
       
-      {
-        l_test_t fz = fx * fy;
-        printf("Try %d * %d: = %d \n", x, y, fz.val);
-        fflush(stdout);
-      }
-
       // if (y == 0) {
       //   continue;
       // }
