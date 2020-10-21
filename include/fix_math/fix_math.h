37bc00a823937896ee7af6787d9d6b5777b2d9d7
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-21 08:26:25 -0400

    ...

--- include/fix_math/fix_math.h
+++ include/fix_math/fix_math.h
@@ -1166,7 +1166,7 @@ namespace lamb {
       if ( sizeof(signed_frac<charac,mantissa>) > sizeof(signed_frac) ) {
         pseudo_right_big_type tmp = ((pseudo_right_big_type)val) * other.val;
         
-        printf("preTMP0    is %d\n", tmp);
+        printf("preTMP0    is %lld\n", tmp);
 
         tmp >>= signed_frac<charac,mantissa>::FX_SHIFT - 1;
 
@@ -1177,7 +1177,7 @@ namespace lamb {
         
         if (tmp > MAX) {
 #ifndef LAMB_FP_SATURATE
-          printf("OVERFLOW: %d * %d = %d\n", val, other.val, tmp);
+          printf("OVERFLOW: %d * %d = %lld\n", val, other.val, tmp);
           fflush(stdout);
 #else
           r.val = MAX;
--- util/test.cpp
+++ util/test.cpp
@@ -10,21 +10,21 @@ using namespace std;
 // Multiply tests:
 //
 // L/R     u0,8   u0,16  u0,32  u8,8  u16,16  s0,7  s0,15  s0,31  s7,8  s15,16
-// u0,8    Yes    Yes    Yes    EXCL  EXCL    OMIT  OMIT   OMIT   EXCL  EXCL
-// u0,16   Yes    Yes    Yes    EXCL  EXCL    OMIT  OMIT   OMIT   EXCL  EXCL
-// u0,32   Yes    Yes    Yes    EXCL  EXCL    OMIT  OMIT   OMIT   EXCL  EXCL
-// u8,8    Yes    Yes    Yes    ?     ?       OMIT  OMIT   OMIT   EXCL  EXCL
-// u16,16  Yes    Yes    Yes    ?     ?       OMIT  OMIT   OMIT   EXCL  EXCL
+// u0,8    Yes    Yes    Yes    EXCL  EXCL    EXCL  EXCL   EXCL   EXCL  EXCL
+// u0,16   Yes    Yes    Yes    EXCL  EXCL    EXCL  EXCL   EXCL   EXCL  EXCL
+// u0,32   Yes    Yes    Yes    EXCL  EXCL    EXCL  EXCL   EXCL   EXCL  EXCL
+// u8,8    Yes    Yes    Yes    ?     ?       EXCL  EXCL   EXCL   EXCL  EXCL
+// u16,16  Yes    Yes    Yes    ?     ?       EXCL  EXCL   EXCL   EXCL  EXCL
 // s0,7    Yes    Yes    Yes    EXCL  EXCL    Yes   Yes    Yes    EXCL  EXCL
 // s0,15   Yes    Yes    Yes    EXCL  EXCL    Yes   Yes    Yes    EXCL  EXCL
-// s0,31   Yes    Yes    Yes    EXCL  EXCL    Yes
+// s0,31   Yes    Yes    Yes    EXCL  EXCL    Yes   Yes    Yes    EXCL  EXCL
 // s7,8    
 // s15,16  
 
 using namespace lamb;
 
 typedef signed_frac<0,31>      l_test_t;
-typedef signed_frac<0,15>      r_test_t;
+typedef signed_frac<0,31>      r_test_t;
 
 #define L_BASE ((l_test_t::MAX >> (sizeof(l_test_t::type) >> 1) >> 3) + 1)
 #define R_BASE ((r_test_t::MAX >> (sizeof(r_test_t::type) >> 1) >> 4) + 1)
