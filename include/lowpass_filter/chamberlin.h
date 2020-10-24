e6a5608a7e153ce2baff3c17ed70e2bcc25c55b0
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-24 07:46:33 -0400

    wip

--- include/lowpass_filter/chamberlin.h
+++ include/lowpass_filter/chamberlin.h
@@ -64,6 +64,71 @@ namespace lamb {
     return sat_q0n15::from_float(L);
    }
   };
+
+  class fx_chamberlin {
+  public:
+   sat_q7n8 Q1, Q, F1, F, FS, D1, D2, L, H, B, N;
+    
+   fx_chamberlin() :
+    Q1(0), Q(0), F1(0), F(0), FS(0), D1(0), D2(0), L(0), H(0), B(0), N(0) {
+    Q.val  = 1.0;
+    F.val  = 1000;
+    FS.val = 44100;
+
+    set_frequency();
+    set_q();
+   }
+
+   void set_frequency() {
+    auto pi2 = sat_q7n8::from_float(2.0 * M_PI);
+    F1       = pi2;
+    F1      *= F / FS;
+    
+
+    printf("F1: % 05.5lf \n", F1.to_float());
+   }
+
+   void set_q() {
+    Q1 = sat_q7n8(sat_q7n8::ONE) / Q;
+    // printf("Q1: % 05.5lf \n", Q1);
+   }
+   
+   sat_q7n8 process(sat_q7n8 input) {
+//    printf("% 5.5lf, ", Q1)    ;
+
+    printf("% 5.5lf, ", F1)    ;
+    
+    sat_q7n8 I = input;
+    printf("% 5.5lf, ", I.to_float())    ;
+    
+//    L = D2 + F1 * D1
+//    H = I - L - Q1*D1
+//    B = F1 * H + D1
+//    N = H + L
+//    D1 = B
+//    D2 = L
+
+    L  = D2 + F1 * D1         ;
+    printf("% 5.5lf, ", L.to_float())    ;
+
+    H  = I  - L  - Q1*D1      ;
+    printf("% 5.5lf, ", H.to_float())    ;
+
+    B  = F1 * H  + D1         ;
+    printf("% 5.5lf, ", B.to_float())    ;
+    
+    N  = H  + L               ;
+    printf("% 5.5lf, ", N.to_float())    ;
+
+    D1 = B                    ;
+    printf("% 5.5lf, ", D1.to_float())   ;
+    
+    D2 = L                    ;
+    printf("% 5.5lf  ", D2.to_float())   ;
+        
+    return L;
+   }
+  };
 }
 
 #endif /* LAMB_CHAMBERLIN_H_ */
--- util/test.cpp
+++ util/test.cpp
@@ -7,30 +7,60 @@ using namespace std;
 using namespace lamb;
 
 int main() {
- chamberlin cf;
-
- sat_q0n15 acc(q0n15::MIN);
-
- printf("F, I, L, H, B, N, D1, D2 \n");
-
- for (size_t qix = 1; qix < 100; qix++) {
-  cf.Q = qix;
-  cf.set_q();
+ {
+  chamberlin cf;
   
-  for (size_t fix = 200; fix > 120; fix -= 5) {
-   cf.F    = fix;
-   acc.val = 0;
+  sat_q0n15 acc(q0n15::MIN);
+  
+  printf("F, I, L, H, B, N, D1, D2 \n");
+  
+  for (size_t qix = 1; qix < 300; qix += 2) {
+   cf.Q = qix;
+   cf.set_q();
    
-   cf.set_frequency();
-   
-   for(size_t ix = 0; ix < 256; ix ++) {
-    acc += 65536L / 128;
+   for (size_t fix = 150; fix > 100; fix -= 10) {
+    cf.F    = fix;
+    acc.val = 0;
     
-    // q0n15_value_type y = (acc.val > 32768u ) ? sat_q0n15::MAX : 0;
-    
-    auto x = cf.process(sat_q0n15(acc));
+    cf.set_frequency();
    
-    printf("\n");
+    for(size_t ix = 0; ix < 64; ix ++) {
+     acc += 65536L / 64;
+     
+     // q0n15_value_type y = (acc.val > 32768u ) ? sat_q0n15::MAX : 0;
+     
+     auto x = cf.process(sat_q0n15(acc));
+     
+     printf("\n");
+    }
+   }
+  }
+
+  {
+   fx_chamberlin cf;
+  
+   sat_q7n8 acc(q7n8::MIN);
+  
+   for (size_t qix = 1; qix < 300; qix += 2) {
+    cf.Q = sat_q7n8::from_float(qix);
+    cf.set_q();
+   
+    for (size_t fix = 150; fix > 100; fix -= 10) {
+     cf.F    = sat_q7n8(fix, 0);
+     acc.val = 0;
+    
+     cf.set_frequency();
+   
+     for(size_t ix = 0; ix < 64; ix ++) {
+      acc += 65536L / 64;
+    
+      // q7n8_value_type y = (acc.val > 32768u ) ? sat_q7n8::MAX : 0;
+    
+      auto x = cf.process(acc);
+    
+      printf("\n");
+     }
+    }
    }
   }
  }
