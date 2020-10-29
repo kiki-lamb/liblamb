e68328f6e4c8f1962cdcab03e6c28fcd16bdf9bf
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-29 02:04:07 -0400

    ..

--- util/fp_sine.cpp
+++ util/fp_sine.cpp
@@ -41,7 +41,7 @@ out_type qsin(in_type const & x_)
  x.value  = x.value           >> shift_2                   ; 
  x.value  = x.value * x.value >> shift_3                   ; // x=x^2 To Q14
  y        = B       - x        * C                         ; 
- y.value  = quarter.value   - (x.value  * y.value >> 16); // A - x^2*(B-x^2*C)
+ y        = quarter -      (x  * y >> 2); // A - x^2*(B-x^2*C)
  
  return out_type(c.value >= 0 ? y.value : -y.value);
 }
