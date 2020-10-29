b5dd31fd3d4b61472ef95784e052f5e66478ad0a
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-29 01:17:06 -0400

    ...

--- util/fp_sine.cpp
+++ util/fp_sine.cpp
@@ -18,18 +18,18 @@ out_type qsin(in_type const & x_)
  int32_t c;
  int32_t y;
  
- constexpr int32_t qN = 13;
- constexpr int32_t qA = 12;
+ constexpr int32_t shift_qcirc = 13;
+ constexpr int32_t shift_out = 12;
  constexpr int32_t B  = 19900;
  constexpr int32_t C  = 3516;
  
- c  = x         << (30 - qN      );  // Semi-circle info into carry.
- x -= 1         << (qN           );  // sine -> cosine calc
- x  = x         << (31 - qN      );  // Mask with PI
- x  = x         >> (31 - qN      );  // Note: SIGNED shift! (to qN)
- x  = (x  * x ) >> (2  * qN  - 14);  // x=x^2 To Q14
- y  = B          - (x  * C  >> 14);  // B - x^2 * C
- y  = (1 << qA)  - (x  * y  >> 16);  // A - x^2*(B-x^2*C)
+ c  = x                 << (30 - shift_qcirc      );  // Semi-circle info into carry.
+ x -= 1                 << (shift_qcirc           );  // sine -> cosine calc
+ x  = x                 << (31 - shift_qcirc      );  // Mask with PI
+ x  = x                 >> (31 - shift_qcirc      ); 
+ x  = (x  * x )         >> (2  * shift_qcirc  - 14);  // x=x^2 To Q14
+ y  = B                  - (x  * C           >> 14);  // B - x^2 * C
+ y  = (1 << shift_out)   - (x  * y           >> 16);  // A - x^2*(B-x^2*C)
  
  return out_type(c>=0 ? y : -y);
 }
