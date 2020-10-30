01cd73e484f704c895f4ae9dfff01d880e5bfe84
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-29 20:00:04 -0400

    revisions

--- util/crtp-test.cpp
+++ util/crtp-test.cpp
@@ -45,7 +45,8 @@ class integer {
 //----------------------------------------------------------------------------------------------------------------------
 public:
  //---------------------------------------------------------------------------------------------------------------------
- typedef value_type_ value_type;
+ typedef integer_traits<value_type_> traits;
+ typedef typename traits::type   value_type;
  //---------------------------------------------------------------------------------------------------------------------
  value_type value;
  //---------------------------------------------------------------------------------------------------------------------
@@ -54,29 +55,31 @@ public:
  //---------------------------------------------------------------------------------------------------------------------
  /**/           operator     value_type(           )    const   = delete;
  //---------------------------------------------------------------------------------------------------------------------
- integer          operator  +  ()                         const   = delete;
- integer          operator  ~  ()                         const   { return integer(       ~ value      ); }
- integer          operator  -  ()                         const   { return integer(       - value      ); }
- integer          operator >>  (uint8_t const & shift)    const   { return integer(value >> shift      ); }
- integer          operator <<  (uint8_t const & shift)    const   { return integer(value << shift      ); }
- integer          operator  +  (integer   const & other)    const   { return integer(value  + other.value); }
- integer          operator  -  (integer   const & other)    const   { return integer(value  - other.value); }
- integer          operator  *  (integer   const & other)    const   { return integer(value  * other.value); }
- integer          operator  /  (integer   const & other)    const   { return integer(value  / other.value); }
- integer          operator  %  (integer   const & other)    const   { return integer(value  % other.value); }
- bool           operator  <  (integer   const & other)    const   { return      (value  < other.value); }
- bool           operator  >  (integer   const & other)    const   { return      (value  > other.value); }
- bool           operator ==  (integer   const & other)    const   { return      (value == other.value); }
+ integer        operator  +  ()                         const   = delete;
+ integer        operator  ~  ()                         const   { return integer(       ~ value      ); }
+ integer        operator  -  ()                         const   { return integer(       - value      ); }
+ //---------------------------------------------------------------------------------------------------------------------
+ integer        operator >>  (uint8_t const & shift)    const   { return integer(value >> shift      ); }
+ integer        operator <<  (uint8_t const & shift)    const   { return integer(value << shift      ); }
+ integer        operator  +  (integer const & other)    const   { return integer(value  + other.value); }
+ integer        operator  -  (integer const & other)    const   { return integer(value  - other.value); }
+ integer        operator  *  (integer const & other)    const   { return integer(value  * other.value); }
+ integer        operator  /  (integer const & other)    const   { return integer(value  / other.value); }
+ integer        operator  %  (integer const & other)    const   { return integer(value  % other.value); }
+ //---------------------------------------------------------------------------------------------------------------------
+ bool           operator  <  (integer const & other)    const   { return        (value  < other.value); }
+ bool           operator  >  (integer const & other)    const   { return        (value  > other.value); }
+ bool           operator ==  (integer const & other)    const   { return        (value == other.value); }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename base>
-class mathematized : public base {
+class mathematized :  public base {
 //----------------------------------------------------------------------------------------------------------------------
 public:
  //---------------------------------------------------------------------------------------------------------------------
- typedef typename base::value_type value_type;
+ typedef typename            base::value_type value_type;
  //--------------------------------------------------------------------------------------------------------------------- 
  explicit
  mathematized                (value_type   const & v) : base(v) {}
@@ -85,13 +88,16 @@ public:
  //----------------------------------------------------------------------------------------------------------------------
  mathematized & operator  -- (                      )           { this        -= mathematized(1);        return *this; }
  mathematized & operator  ++ (                      )           { this        += mathematized(1);        return *this; }
+  //---------------------------------------------------------------------------------------------------------------------
  mathematized & operator >>= (uint8_t      const & v)           { this->value  = (*this    >> v ).value; return *this; }
  mathematized & operator <<= (uint8_t      const & v)           { this->value  = (*this    << v ).value; return *this; }
+  //---------------------------------------------------------------------------------------------------------------------
  mathematized & operator  -= (mathematized const & v)           { this->value  = (*this     - v ).value; return *this; }
  mathematized & operator  += (mathematized const & v)           { this->value  = (*this     + v ).value; return *this; }
  mathematized & operator  *= (mathematized const & v)           { this->value  = (*this     * v ).value; return *this; }
  mathematized & operator  /= (mathematized const & v)           { this->value  = (*this     / v ).value; return *this; }
  mathematized & operator  %= (mathematized const & v)           { this->value  = (*this     % v ).value; return *this; }
+  //---------------------------------------------------------------------------------------------------------------------
  bool           operator  <= (mathematized const & o)   const   { return         (*this    == o )     || (*this < o) ; }
  bool           operator  >= (mathematized const & o)   const   { return         (*this    == o )     || (*this > o) ; }
  bool           operator  != (mathematized const & o)   const   { return       ! (*this    == o )                    ; } 
@@ -101,17 +107,17 @@ public:
 
 int main() {
  typedef mathematized<integer<uint8_t>> t;
+
  t x(7);
  t y(9);
  t z(8);
 
- 
- printf("=> %u \n", (x + y).value);
+ printf("=> %lf \n", (x + y).value);
 
  x += y;
  x += z;
 
- printf("=> %u \n", x.value);
+ printf("=> %lf \n", x.value);
 
  if constexpr(integer_traits<uint16_t>::SIGNED) {
   printf("YES\n");
