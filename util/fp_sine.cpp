9c92c82d930a34fd36e6244fd88230d1c89f43c2
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-30 21:19:01 -0400

    ...

--- util/fp_sine.cpp
+++ util/fp_sine.cpp
@@ -56,24 +56,24 @@ namespace math {
    constexpr mid_type B       { two        - pi / 4.0_mid        };
    constexpr mid_type C       { one        - pi / 4.0_mid        };
 
-   printf("%12lu, ", x_);
+   printf("%8d, ", x_);
    
    //--------------------------------------------------------------
-   mid_type           x       { x_        << q_shift - 1         }; printf("%12lu, ", x);
-   mid_type           cry     { x         << q_shift             }; printf("%12lu, ", cry);
+   mid_type           x       { x_        << q_shift - 1         }; printf("%8d, ", x);
+   mid_type           cry     { x         << q_shift             }; printf("%8d, ", cry);
    //--------------------------------------------------------------
-   x                          = x          - half                 ; printf("%12lu, ", x);
-   x                          = x         << q_shift + 1          ; printf("%12lu, ", x);
-   x                          = x         >> q_shift + 1          ; printf("%12lu, ", x);
-   x                          = x          * x                    ; printf("%12lu, ", x);
-   x                          = x         << 2                    ; printf("%12lu, ", x);
+   x                          = x          - half                 ; printf("%13d, ", x);
+   x                          = x         << q_shift + 1          ; printf("%13d, ", x);
+   x                          = x         >> q_shift + 1          ; printf("%13d, ", x);
+   x                          = x          * x                    ; printf("%13d, ", x);
+   x                          = x         << 2                    ; printf("%13d, ", x);
    //--------------------------------------------------------------
-   mid_type           y       { x          * C                   }; printf("%12lu, ", y);
+   mid_type           y       { x          * C                   }; printf("%13d, ", y);
    //--------------------------------------------------------------
-   y                          = B          - y                    ; printf("%12lu, ", y);
-   y                          = y          * x                    ; printf("%12lu, ", y);
-   y                          = one        - y                    ; printf("%12lu, ", y);
-   y                          = cry.value >= 0 ? y : -y           ; printf("%12lu, ", y);
+   y                          = B          - y                    ; printf("%13d, ", y);
+   y                          = y          * x                    ; printf("%13d, ", y);
+   y                          = one        - y                    ; printf("%13d, ", y);
+   y                          = cry.value >= 0 ? y : -y           ; printf("%13d, ", y);
    //--------------------------------------------------------------
    return                       out_type(y)                       ;
   }
