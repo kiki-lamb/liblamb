fe0167c95ea6d6b14943fee6edddf352a4665511
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-30 22:46:22 -0400

    little better, use small_one in just one spot.

--- util/fp_sine.cpp
+++ util/fp_sine.cpp
@@ -52,13 +52,14 @@ namespace math {
 
    //if (! logged) printf("small one: % 8d \n", u8q8::ONE);
    
-   constexpr uint8_t  q_shift { mid_type::WHOLE                  };   
-   constexpr mid_type pi      { mid_type::PI                     }; //if (! logged) printf("pi:   % 8d % 05.5lf \n", pi,   double(pi));
-   constexpr mid_type one     { (1.0_mid).value - 1              }; //if (! logged) printf("one:  % 8d % 05.5lf \n", one,  double(one));
-   constexpr mid_type two     { one << 1                         }; //if (! logged) printf("two:  % 8d % 05.5lf \n", two,  double(two));
-   constexpr mid_type half    { one >> 1                         }; //if (! logged) printf("half: % 8d % 05.5lf \n", half, double(half));
-   constexpr mid_type B       { two        - pi / 4.0_mid        }; //if (! logged) printf("B:    % 8d % 05.5lf \n", B,    double(B));
-   constexpr mid_type C       { one        - pi / 4.0_mid        }; //if (! logged) printf("C:    % 8d % 05.5lf \n", C,    double(C));   
+   constexpr uint8_t  q_shift   { mid_type::WHOLE                  };   
+   constexpr mid_type pi        { mid_type::PI                     }; //if (! logged) printf("pi:   % 8d % 05.5lf \n", pi,   double(pi));
+   constexpr mid_type one       { 1.0_mid                          }; //if (! logged) printf("one:  % 8d % 05.5lf \n", one,  double(one));
+   constexpr mid_type small_one { (1.0_mid).value - 1              }; //if (! logged) printf("one:  % 8d % 05.5lf \n", one,  double(one));
+   constexpr mid_type two       { one << 1                         }; //if (! logged) printf("two:  % 8d % 05.5lf \n", two,  double(two));
+   constexpr mid_type half      { one >> 1                         }; //if (! logged) printf("half: % 8d % 05.5lf \n", half, double(half));
+   constexpr mid_type B         { two        - pi / 4.0_mid        }; //if (! logged) printf("B:    % 8d % 05.5lf \n", B,    double(B));
+   constexpr mid_type C         { one        - pi / 4.0_mid        }; //if (! logged) printf("C:    % 8d % 05.5lf \n", C,    double(C));   
    logged = true;
    
    //printf("% 6d, ", x_);
@@ -78,7 +79,7 @@ namespace math {
    //--------------------------------------------------------------                      
    y                          = B          - y                    ; //printf("% 13d, ", y);  
    y                          = y          * x                    ; //printf("% 13d, ", y);  
-   y                          = one        - y                    ; //printf("% 13d, ", y);  
+   y                          = small_one  - y                    ; //printf("% 13d, ", y);  
    y                          = cry.value >= 0 ? y : -y           ; //printf("% 13d, ", y);  
    //--------------------------------------------------------------
    return                       out_type(y)                       ;
