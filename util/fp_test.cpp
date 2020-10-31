91d033050b877031f86ed16a79a2c4cf8f26963d
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-31 01:18:47 -0400

    ...

--- util/fp_test.cpp
+++ util/fp_test.cpp
@@ -30,4 +30,5 @@ int main() {
  // printf("x:    % 8d % 010.10lf \n", float(s15q16::ffrom_float(1.5)));
  printf("x:    % 10.10lf \n", float(s15q16::ffrom_float(1.5).scaled_add(s17q14::ffrom_float(1.5))));
  printf("x:    % 10.10lf \n", float(s15q16::ffrom_float(1.5) + s17q14::ffrom_float(1.5)));
+ printf("x:    % 10.10lf \n", float(s15q16::ffrom_float(2.0) + s17q14::ffrom_float(3.0)));
 }
