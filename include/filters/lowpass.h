51075fa522cc033ef1e3a26965f05b9423c20ba5
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-03 19:11:48 -0500

    refactors

--- include/filters/lowpass.h
+++ include/filters/lowpass.h
@@ -2,10 +2,9 @@
 #define LOWPASS_LAMB_H_
 
 namespace lamb {
-  template <typename sample_t_>
   class lowpass {
   public:
-    typedef sample_t_                                            sample_t;
+    typedef int16_t                                              sample_t;
     typedef typename sample_type_traits<sample_t>::unsigned_type unsigned_sample_t;
     typedef typename sample_type_traits<sample_t>::mix_type      mix_t;
     typedef typename sample_type_traits<mix_t>::unsigned_type    unsigned_mix_t;
@@ -15,7 +14,7 @@ namespace lamb {
         typename sample_type_traits<sample_t>::unmixed_type
       ) << 3)>                                                   control_frac_t;
 
-    typedef typename control_frac_t::value_type                        control_t;
+    typedef typename control_frac_t::value_type                  control_t;
 
     static const control_t control_t_one = control_frac_t::ONE.value;
 
@@ -34,48 +33,45 @@ namespace lamb {
   public:
     lowpass() : _mode(LP) {}
 
-    mode_t mode() const {
+    inline mode_t mode() const {
       return _mode;
     }
 
-    control_t freq() const {
+    inline control_t freq() const {
       return _freq;
     }
     
-    control_t q() const {
+    inline control_t q() const {
       return _q;
     }
 
-    void mode(mode_t const & mode_) {
+    inline void mode(mode_t const & mode_) {
       _mode = mode_;
     }
     
-    void freq(control_t const & freq_) {
+    inline void freq(control_t const & freq_) {
       _freq     = freq_;
-      _feedback = q() + us_fxmul_cXc(q(), control_t_one - _freq);
+      auto tmp  = ((unsigned_mix_t(q())) * (control_t_one - _freq)) >> FX_SHIFT;
+      _feedback = q() + tmp;
     }
 
-    void q(control_t const & q_) {
+    inline void q(control_t const & q_) {
       _q = q_;
     }
 
-    sample_t process(sample_t const & in_) {
+    inline sample_t process(sample_t const & in_) {
       // D0 = D0 + FREQ * (IN - D0 + FB * (D0 - O));
       // O  = O  + FREQ * (D0 - O);
 
       sample_t hp = in_ - _d0;
+
+      auto tmp = (_feedback * (_d0 - _o)) >> FX_SHIFT;
       
-      _d0 += m_fxmul_mXs(
-        hp + m_fxmul_mXs(_feedback, _d0 - _o),
-        freq()
-      );
+      _d0 += ((hp + tmp) * freq()) >> FX_SHIFT;
       
       sample_t bp = _d0 - _o;
 
-      _o += s_fxmul_sXc(
-        bp,
-        freq()
-      ); 
+      _o += (bp * freq()) >> FX_SHIFT;
 
       if      (_mode == HP)
         return hp;
@@ -83,19 +79,6 @@ namespace lamb {
         return bp;
       
       return _o;
-    }
-
-  private:
-    static unsigned_sample_t us_fxmul_cXc(control_t const & a, control_t const & b) {
-      return ((unsigned_mix_t)a * b) >> FX_SHIFT;
-    }
-    
-    static sample_t s_fxmul_sXc(sample_t const & a, control_t const & b) {
-      return (a * b) >> FX_SHIFT;
-    }
-    
-    static mix_t m_fxmul_mXs(mix_t const & a, sample_t const & b) {
-      return (a * b) >> FX_SHIFT; // Note that FX_SHIFT is less than size of input!
     }
   };
 }
