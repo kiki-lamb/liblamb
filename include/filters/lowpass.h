bb7cf29137f4cca008bf05bd505aa69cedb8a266
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-04 07:16:52 -0500

    seems okay

--- include/filters/lowpass.h
+++ include/filters/lowpass.h
@@ -8,7 +8,7 @@ namespace lamb {
 
   typedef s0q15                                                io_t;
 
-  enum mode_t { LP, BP, HP };
+//  enum mode_t { LP, BP, HP };
 
  private:
 
@@ -16,7 +16,7 @@ namespace lamb {
 
  public:
   
-  typedef typename control_frac_t::value_type                  control_t;
+//  typedef typename control_frac_t::value_type                  control_t;
   
  private:
 
@@ -25,44 +25,49 @@ namespace lamb {
   typedef typename sample_type_traits<sample_t>::mix_type      mix_t;
   typedef typename sample_type_traits<mix_t>::unsigned_type    unsigned_mix_t;
 
-  static const control_t control_t_one = control_frac_t::ONE.value;
+  static constexpr control_frac_t control_t_one = control_frac_t::ONE;
 
  private:
-  static const uint8_t FX_SHIFT = sizeof(control_t) << 3;
+  static constexpr uint8_t FX_SHIFT = 8;
     
-  control_t         _q;
-  control_t         _freq;
+  control_frac_t    _q;
+  control_frac_t    _freq;
   unsigned_sample_t _feedback;
   sample_t          _d0;
   sample_t          _o;
-  mode_t            _mode;
+//  mode_t          _mode;
     
  public:
-  lowpass() : _mode(LP) {}
+  lowpass() /* : _mode(LP) */ {}
 
-  inline mode_t mode() const {
-   return _mode;
-  }
+  // inline mode_t mode() const {
+  //  return _mode;
+  // }
 
-  inline control_t freq() const {
+  inline control_frac_t freq() const {
    return _freq;
   }
     
-  inline control_t res() const {
+  inline control_frac_t res() const {
    return _q;
   }
 
-  inline void mode(mode_t const & mode_) {
-   _mode = mode_;
-  }
+  // inline void mode(mode_t const & mode_) {
+  //  _mode = mode_;
+  // }
     
-  inline void freq(control_t const & freq_) {
+  inline void freq(control_frac_t const & freq_) {
    _freq     = freq_;
-   auto tmp  = ((unsigned_mix_t(res())) * (control_t_one - _freq)) >> FX_SHIFT;
-   _feedback = res() + tmp;
+
+   auto tmp  = (
+    (unsigned_mix_t(res().value)) *
+    (control_t_one - _freq).value
+   ) >> FX_SHIFT;
+   
+   _feedback = (res() + tmp).value;
   }
 
-  inline void res(control_t const & q_) {
+  inline void res(control_frac_t const & q_) {
    _q = q_;
   }
 
@@ -90,16 +95,16 @@ namespace lamb {
    auto tmp1 = _d0 - _o;                             ////snprintf(buff, 64, "% 9.9fa, ", float(s0q15(tmp1))); Serial.print(buff);
    auto tmp2 = (_feedback * tmp1) >> FX_SHIFT;       ////snprintf(buff, 64, "% 9.9fb, ", float(s0q15(tmp2))); Serial.print(buff);
       
-   _d0 += ((hp + tmp2) * freq()) >> FX_SHIFT;        ////snprintf(buff, 64, "% 9.9fc, ", float(s0q15(_d0))); Serial.print(buff);
+   _d0 += ((hp + tmp2) * freq().value) >> FX_SHIFT;        ////snprintf(buff, 64, "% 9.9fc, ", float(s0q15(_d0))); Serial.print(buff);
       
    sample_t bp = _d0 - _o;                           ////snprintf(buff, 64, "% 9.9f, ", float(s0q15(bp))); Serial.print(buff);
 
-   _o += (bp * freq()) >> FX_SHIFT;                  ////snprintf(buff, 64, "% 9.9f, ", float(s0q15(_o))); Serial.print(buff);
+   _o += (bp * freq().value) >> FX_SHIFT;                  ////snprintf(buff, 64, "% 9.9f, ", float(s0q15(_o))); Serial.print(buff);
 
-   if      (_mode == HP)
-    return io_t(hp);
-   else if (_mode == BP)
-    return io_t(bp);
+   // if      (_mode == HP)
+   //  return io_t(hp);
+   // else if (_mode == BP)
+   //  return io_t(bp);
 
    io_t out(_o);
 
