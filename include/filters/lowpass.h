776e2046db0f4d86a21d8358b8870d1158de2ccb
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-05 23:03:32 -0500

    seems to perform okay

--- include/filters/lowpass.h
+++ include/filters/lowpass.h
@@ -8,11 +8,11 @@ namespace lamb {
  /////////////////////////////////////////////////////////////////////////////////////////
 
  template <
-  typename internal_t          = s16p0q15,
-  typename unsigned_internal_t = u16p0q16,
   typename external_t          = s0q15,
-  bool use_limits = true
-  >  // tested with s0q15 and s0q31
+  bool use_limits              = true,
+  typename internal_t          = s16p0q31,
+  typename unsigned_internal_t = u16p0q32
+  >
  class lowpass {
 
   //--------------------------------------------------------------------------------------
