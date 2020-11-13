2471495c81e2d4f65223cebe16f4c45c93c9037f
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-12 20:19:43 -0500

    variable data size for events.

--- include/events/event.h
+++ include/events/event.h
@@ -5,27 +5,37 @@
 
 namespace lamb {
   namespace events {
-    template <typename enum_t_>
-    class event {
+   template <
+    typename enum_t_,
+    typename data_t_ = uint16_t,
+    uint8_t bottom_bits_ = sizeof(data_t_) * 4,
+    uint8_t top_bits_ = sizeof(data_t_) * 4
+    >
+   class event {
     public:
+    typedef enum_t_ enum_t;
+    typedef data_t_ data_t;
+    static constexpr uint8_t top_bits = top_bits_;
+    static constexpr uint8_t bottom_bits = bottom_bits_;
+    
       typedef enum_t_ event_type;   
     
       event_type type;
-      uint16_t parameter;
+      data_t parameter;
 
       inline event(
         event_type type_ = ((event_type)0),
-        uint16_t parameter_ = 0
+        data_t parameter_ = 0
       ) :
         type(type_),
         parameter(parameter_) {}
     
-      inline uint8_t parameter_hi() const {
-        return parameter >> 8;
+      inline data_t parameter_hi() const {
+       return parameter >> bottom_bits;
       }
 
-      inline uint8_t parameter_lo() const {
-        return parameter & 0xff;
+      inline data_t parameter_lo() const {
+       return parameter << top_bits >> top_bits;
       }
 
       inline operator bool() const {
