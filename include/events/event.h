82da3bdf52b5508695c1560375148011edf80e64
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-12 20:24:44 -0500

    ...

--- include/events/event.h
+++ include/events/event.h
@@ -4,45 +4,47 @@
 #include <inttypes.h>
 
 namespace lamb {
-  namespace events {
-   template <
-    typename enum_t_,
-    typename data_t_ = uint16_t,
-    uint8_t bottom_bits_ = sizeof(data_t_) * 4,
-    uint8_t top_bits_ = sizeof(data_t_) * 4
-    >
-   class event {
-    public:
-    typedef enum_t_ enum_t;
-    typedef data_t_ data_t;
-    static constexpr uint8_t top_bits = top_bits_;
-    static constexpr uint8_t bottom_bits = bottom_bits_;
-    
-      typedef enum_t_ event_type;   
-    
-      event_type type;
-      data_t parameter;
+ namespace events {
+  template <
+   typename enum_t_,
+   typename data_t_ = uint16_t,
+   uint8_t bottom_bits_ = sizeof(data_t_) * 4,
+   uint8_t top_bits_ = sizeof(data_t_) * 4
+   >
+  class event {
+  public:
+   typedef enum_t_ enum_t;
+   typedef data_t_ data_t;
 
-      inline event(
-        event_type type_ = ((event_type)0),
-        data_t parameter_ = 0
-      ) :
-        type(type_),
-        parameter(parameter_) {}
+   static constexpr uint8_t top_bits = top_bits_;
+   static constexpr uint8_t bottom_bits = bottom_bits_;
+   static constexpr data_t  bottom_mask = ((uint64_t)1 << bottom_bits) - 1;
     
-      inline data_t parameter_hi() const {
-       return parameter >> bottom_bits;
-      }
+   typedef enum_t_ event_type;   
+    
+   event_type type;
+   data_t parameter;
 
-      inline data_t parameter_lo() const {
-       return parameter << top_bits >> top_bits;
-      }
+   inline event(
+    event_type type_ = event_type(0),
+    data_t parameter_ = data_t()
+   ) :
+    type(type_),
+    parameter(parameter_) {}
+    
+   inline data_t parameter_hi() const {
+    return parameter >> bottom_bits;
+   }
 
-      inline operator bool() const {
-        return type != ((event_type)0);
-      }
-    };
-  }
+   inline data_t parameter_lo() const {
+    return parameter& bottom_mask;
+   }
+    
+   inline operator bool() const {
+    return type != ((event_type)0);
+   }
+  };
+ }
 }
 
 #endif
