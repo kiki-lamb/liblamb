049125462ba3f193b6e77c8846f0de34fe91bc4f
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-24 19:10:48 -0500

    PT8211 should only touch _spi.

--- include/device/pt8211/pt8211.h
+++ include/device/pt8211/pt8211.h
@@ -3,51 +3,51 @@
 
 #include "SPI.h"
 #include <Arduino.h>
-#include "../../sample_type_interfaces/sample_type_interfaces.h"
 
 namespace lamb {
-  namespace device {
-    class pt8211 : public sample_sink<s0q15> {
-    public:
-      inline pt8211(unsigned int ws_pin_, SPIClass * spi_ = nullptr) :
-        _spi(spi_),
-        _ws_pin(ws_pin_) {}      
+ namespace device {
+  class pt8211 : public sample_sink<s0q15> {
+  public:
+   inline pt8211(unsigned int ws_pin_) :
+    _spi(nullptr),
+    _ws_pin(ws_pin_) {}      
       
-      inline void setup(SPIClass * spi_ = nullptr) {
-        if (spi_ != nullptr)
-          _spi = spi_;
-        
-        _spi->setBitOrder(MSBFIRST);
-        _spi->setDataMode(SPI_MODE0);
-        _spi->setClockDivider(SPI_CLOCK_DIV2);
-        
-        pinMode(_ws_pin, OUTPUT);
-      }
-
-      inline void write_mono(s0q15 const & sample) {
-        _lData   = sample.value;
-        _hData   = sample.value;
-        _hData >>= 8;
-        
-        digitalWrite(_ws_pin, LOW);
-        SPI.transfer(_hData);
-        SPI.transfer(_lData);
-        
-        digitalWrite(_ws_pin, HIGH);
-      }
-
-    protected:
-      inline virtual void impl_sink(s0q15 const & val) {
-        write_mono(val);
-      }
+   inline void setup(SPIClass & spi_) {
+    _spi = &spi_;
       
-    private:
-      SPIClass *   _spi;
-      uint16_t     _hData;
-      uint16_t     _lData;
-      unsigned int _ws_pin;      
-    };
-  }
+    _spi->setBitOrder(MSBFIRST);
+    _spi->setDataMode(SPI_MODE0);
+    _spi->setClockDivider(SPI_CLOCK_DIV2);
+      
+    pinMode(_ws_pin, OUTPUT);
+   }
+     
+   inline void write_mono(s0q15 const & sample) {
+    _lData   = sample.value;
+
+    _hData   = sample.value;
+    _hData >>= 8;
+        
+    digitalWrite(_ws_pin, LOW);
+
+    _spi->transfer(_hData);
+    _spi->transfer(_lData);
+        
+    digitalWrite(_ws_pin, HIGH);
+   }
+
+  protected:
+   inline virtual void impl_sink(s0q15 const & val) {
+    write_mono(val);
+   }
+      
+  private:
+   SPIClass *   _spi;
+   uint16_t     _hData;
+   uint16_t     _lData;
+   unsigned int _ws_pin;      
+  };
+ }
 }
 
 #endif
